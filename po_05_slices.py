881a2ecba9d3ca8e17e10b3f469fbd2d00cecfac
yand <yand@outlook.com>
2022-08-10 11:12:32 +0300

    YA

---
+++ po_05_slices.py
@@ -0,0 +1,10 @@
+s = "Hello, world!"
+
+print(s[1]) #e
+print(s[1:3]) #el
+print(s[:3]) #Hel
+print(s[0:7:2]) #Hlo
+print(s[::2]) #Hlo ol!
+print(s[:-2]) #Hello, worl
+print(s[-2]) #d
+print(s[::-1]) # !dlrow ,olleH
